ae163e1a766753c543dac7778dc56f3569785e8b
Sean Huff <swhufnagel@gmail.com>
2019-04-23 21:32:34 -0600

    scripts

---
+++ assets/javascript/project.js
@@ -0,0 +1,17 @@
+
+
+$("button").on("click", function() {
+    var animal = $(this).attr("data-animal");
+    var queryURL = "https://api.spotify.com/search?q=" +
+      animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      // Step 1: Run this file, click a button, and see what the response object looks like in the browser's console.
+      // Open up the data key, then open up the 0th, element. Study the keys and how the JSON is structured.
+
+      console.log(response);
+    })
+})
