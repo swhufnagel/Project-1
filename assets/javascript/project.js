b1a124674e0f01068b09b360bd71b20ea754e2e9
Devin Powell <dpowel11@kent.edu>
2019-04-30 18:33:21 -0600

    Added Tables for import

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -52,7 +52,7 @@ function buildQueryURL() {
   // add code here to built out the searchURL
   // the searchType can later be modifed to call the function with different searches
   // ie 'search' or 'seed'
-  queryURL = "https://api.spotify.com/v1/search/q=" + $('#searchTerm').val().trim() + "&type=artist";
+  queryURL = "https://api.spotify.com/v1/search?q=" + $('#searchTerm').val().trim() + "&type=artist";
 
 }
 
@@ -73,7 +73,110 @@ $("#search-button").on("click", function() {
       },
       url: queryURL,
     }).then(function(response) {
-      console.log(response);
-    })
-})
+      // console.log('response: ', response);
+
+      // This code is for the direct response table, not for the recommendations table
+      // Variable Definition and Troubleshooting
+      // Name
+      var resultArtist = response.artists.items["0"].name;
+      // console.log('name: ', resultArtist);
+      // Image
+      var resultImg = response.artists.items["0"].images[0].url;
+      // console.log('image: ', resultImg);
+      // Artist ID:
+      var resultId = response.artists.items["0"].id;
+      console.log('artist id: ', resultId);
+      // Popularity Rating: 
+      var resultPopularity = response.artists.items["0"].popularity;
+      // console.log('popularity: ', resultPopularity);
+      // Genres: 
+      var resultGenre = response.artists.items["0"].genres;
+      // console.log('genres: ', resultGenre);
+
+      // Create variables for pushing to HTML
+      // New Row Variable
+      var newRow = $('<tr>');
+      // New Artist Variable
+      var newArtist = $('<td>');
+      // Add Class to newArtist
+      newArtist.addClass('artist');
+      // Create <td> for img
+      var newImgTag = $('<td>');
+      // Add Class to newImgTag
+      newImgTag.addClass('image');
+      // New Image Variable
+      var newImg = $('<img>');
+      // Add Class to Image
+      newImg.addClass('artistImg');
+      // Store Image in Variable
+      newImg.attr('src', resultImg);
+      // Append newImg to newImgTag
+      newImgTag.append(newImg);
+      // Store Artist Name in variable
+      newArtist.append(resultArtist);
+      // New Genre Variable
+      var newGenre = $('<td class="genre">');
+      newGenre.append(resultGenre);
+      // New Popularity Variable
+      var newPopularity = $('<td class="popularity">');
+      newPopularity.append(resultPopularity);
+
+      // Append to newRow
+      newRow.append(newImg);
+      newRow.append(newArtist);
+      newRow.append(newGenre);
+      newRow.append(newPopularity);
+
+      // Prepend to HTML
+      $('#newTrackRow').prepend(newRow);
+
+      // This code is for appending the RECOMMENDED songs to the appropriate table
+      // Variable Declaration/Troubleshooting/Locations
+      // Path for Image
+      var recResultImage
+      // Path for Song Name
+      var recResultTitle
+      // Path for Artist
+      var recResultArtist
+      // Path for Album
+      var recResultAlbum
+      // Path for Duration
+      var recResultDuration
+      
+      // Create Variable for Recommendation Row
+      var recRow = $('<tr>');
+      // Create Play Button
+      var recPlay = $('<td>');
+      recPlay.addClass('recPlay');
+      // Create Recommended Image Tag
+      var recImageTag = ('<td>');
+      recImageTag.addClass('image');
+      var recImage = ('<img>');
+      recImage.addClass('recImage');
+      // recImage.attr('src', ############);
+      // Create Variable for Title <td>
+      var recTitle = $('<td>');
+      recTitle.addClass('recTitle');
+      // Create Variable for Artist Tag
+      var recArtist = $('<td>');
+      recArtist.addClass('recArtist');
+      // Create Variable for Album Tag
+      var recAlbum = $('<td>');
+      recAlbum.addClass('recAlbum');
+      // Create Variable for Duration
+      var recDuration = $('<td>');
+      recDuration.addClass('recDuration');
+      // Create Variable for Add to Playlist
+      var recAdd = $('<td>');
+      recAdd.addClass('recAdd');
+    
+
+      // Function when the user clicks on a row
+      $('tr').on('click', function() {
+        console.log('clicked a row');
+      });
+    });
+});
+
+
 
