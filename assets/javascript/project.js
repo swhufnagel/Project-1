c9d1101b05db04117f5e78b07909766ec4687755
Sean Huff <swhufnagel@gmail.com>
2019-05-03 23:13:57 -0600

    final working

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -49,7 +49,7 @@ $('#login-button').on('click', function () {
   var scope = "user-library-modify";
   window.location.href = 'https://accounts.spotify.com/authorize?client_id=6ba0c775865d4f34a62198bacaebc943&response_type=token&scope=' + encodeURIComponent(scope) + '&redirect_uri=' + redirectUri;
 })
-$(document).on("click", "#search-button", function () {
+function search(){
   buildQueryURL()
   if (searchTerm !== "") {
     $('.searchTable').removeAttr("style");
@@ -65,11 +65,26 @@ $(document).on("click", "#search-button", function () {
     },
     url: queryURL,
   }).fail(function (jqXHR, textStatus, errorThrown) {
-
     location.reload();
   }).done(function (response) {
+    console.log(response);
+    if(response.artists.total === 0){
+      $('.searchTable').removeAttr("style");
+      $('.searchTable').attr('style', "display:none");
+      $('#clear').removeAttr("style");
+      $('#clear').attr('style', "display:none");
+      $('#more').removeAttr("style");
+      $('#more').attr('style', "display:none");
+      var noneFound = $('#nothing');
+      noneFound.html("No artists found please try again!");
+      noneFound.removeAttr("style");
+      noneFound.attr("style","display:block");
+    }
+    else{
+      $('#nothing').removeAttr("style");
+      $('#nothing').attr("style","display:none");
     // Display Artists that match what the user searched for
-    $("#newTrackRow").empty()
+    $("#newTrackRow").empty();
     var limit = response.artists.items.length;
     if (limit > 4) {
       limit = 4;
@@ -79,13 +94,12 @@ $(document).on("click", "#search-button", function () {
       try {
         var resultImg = response.artists.items[resultNum].images[0].url;
       } catch (error) {
-        resultImg = "/assets/images/artistPlaceholder.png"
+        resultImg = "assets/images/artistPlaceholder.png"
         console.log("An image was not found: " + error)
       }
 
       // Artist ID
       var resultId = response.artists.items[resultNum].id;
-      var resultPopularity = response.artists.items[resultNum].popularity;
       var resultGenre = response.artists.items[resultNum].genres;
 
       // Create a new <tr> and all <td> needed for the results section
@@ -96,13 +110,10 @@ $(document).on("click", "#search-button", function () {
       var newArtist = $('<td>');
       newArtist.addClass('artist');
       newArtist.attr("id","artistText")
-      // var newImgTag = $('<td>');
-      // newImgTag.addClass('image');
       var newImg = $('<img>');
       newImg.addClass('artistImg');
       newImg.attr("id", "artistPic");
       newImg.attr('src', resultImg);
-      // newImgTag.append(newImg);
       newArtist.append(resultArtist);
       var newGenre = $('<td class="genre">');
       newGenre.attr("id","genreText")
@@ -118,9 +129,6 @@ $(document).on("click", "#search-button", function () {
           newGenre.append(resultGenre[i].charAt(0).toUpperCase() + resultGenre[i].slice(1) + " | ");
         }
       }
-      // var newIdtag = $("<td>");
-      // newIdtag.addClass("id");
-      // newIdtag.append(resultId);
       // Append to newRow
       newRow.append(newImg);
       newRow.append(newArtist);
@@ -129,7 +137,86 @@ $(document).on("click", "#search-button", function () {
       // Append to HTML
       $('#newTrackRow').append(newRow);
     }
+  }
   });
+};
+function buildTable(){
+  $("#recommendations").empty();
+  var queryURL = "https://api.spotify.com/v1/recommendations?limit=10&offset=10&market=US&seed_artists=" + artistId;
+  $.ajax({
+    method: "GET",
+    beforeSend: function (request) {
+      request.setRequestHeader("Authorization", authorizationToken);
+      request.setRequestHeader("Accept", "application/json");
+    },
+    url: queryURL,
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+  }).done(function (response) {
+  for (var i = 0; i < 10; i++) {
+    console.log(response);
+    // Create Variables for Recommendation Row
+    var imageSource = response.tracks[i].album.images[1].url;
+    var songTitle = response.tracks[i].name;
+    songTitle = songTitle.substring(0, 25);
+    var songArtist = response.tracks[i].artists[0].name;
+    var albumTitle = response.tracks[i].album.name;
+    var recRow = $('<tr>');
+    recRow.addClass("recRow")
+    var favCol = $("<td>");
+    favCol.attr("id","favoriteCol");
+    favButton = $("<i>");
+    favButton.addClass("far fa-heart");
+    favButton.attr("id", "favorite");
+    favButton.attr("song-id", response.tracks[i].id);
+    favCol.append(favButton);
+    // Create Variable for Image <td>
+    var recImage = $('<img>').attr('src', imageSource)
+    recImage.addClass('recImg');
+    // recImage.append($('<img>').attr('src', imageSource))
+
+    // Create Variable for Title <td>
+    var recTitle = $('<td>');
+    recTitle.addClass('recTitle');
+    recTitle.text(songTitle);
+    recTitle.attr("id", "recTitle")
+    // Create Variable for Artist <td>
+    var recArtist = $('<td>');
+    recArtist.addClass('recArtist');
+    recArtist.text(songArtist);
+    recArtist.attr("id", "recArtist");
+    // Create Variable for Album <td>
+    var recAlbum = $('<td>');
+    recAlbum.addClass('recAlbum');
+    recAlbum.text(albumTitle)
+    recAlbum.attr("id", "recAlbum")
+    var recPlay = $('<td>');
+    recPlay.attr("id","playerCol");
+    var playUri = response.tracks[i].uri;
+    playUri = playUri.substring(playUri.lastIndexOf(":") + 1);
+    var src = "https://open.spotify.com/embed/track/" + playUri
+    var player = $('<iframe id="playSong" width="80" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+    player.attr("src", src);
+    recPlay.append(player);
+    // Append to newRow
+    recRow.append(favCol);
+    recRow.append(recPlay);
+    recRow.append(recTitle);
+    recRow.append(recArtist);
+    recRow.append(recAlbum);
+    $('#recommendations').append(recRow);
+  }
+})
+};
+var limit = 10;
+var artistId;
+$(document).on("click", "#search-button", function () {
+  search();
+});
+document.addEventListener('keypress', function (e) {
+  var key = e.which || e.keyCode;
+  if (key === 13) {
+    search();
+  }
 });
 $(document).on("click", "#favorite", function () {
   $(this).removeClass("far fa-heart");
@@ -151,7 +238,13 @@ $(document).on("click", "#favorite", function () {
 $(document).on('click', '.search-result', function () {
   $('.searchTable').removeAttr("style");
   $('.searchTable').attr('style', "display:none");
-  var artistId = $(this).attr('id');
+  $('#clear').removeAttr("style");
+  $('#clear').attr('style', "display:block");
+  $('#more').removeAttr("style");
+  $('#more').attr('style', "display:block");
+  
+  
+  artistId = $(this).attr('id');
   var queryURL = "https://api.spotify.com/v1/recommendations?limit=10&market=US&seed_artists=" + artistId;
   $.ajax({
     method: "GET",
@@ -163,13 +256,12 @@ $(document).on('click', '.search-result', function () {
   }).fail(function (jqXHR, textStatus, errorThrown) {
   }).done(function (response) {
     $('#recommendations').empty();
-    for (var i = 0; i < response.tracks.length; i++) {
-      console.log(response.tracks[i]);
+    for (i = 0; i < limit; i++) {
+      console.log(response);
       // Create Variables for Recommendation Row
       var imageSource = response.tracks[i].album.images[1].url;
       var songTitle = response.tracks[i].name;
       songTitle = songTitle.substring(0, 25);
-      console.log(songTitle);
       var songArtist = response.tracks[i].artists[0].name;
       var albumTitle = response.tracks[i].album.name;
       var recRow = $('<tr>');
@@ -221,3 +313,16 @@ $(document).on('click', '.search-result', function () {
     $("#recommended-artists").attr('style', 'display:block')
   })
 });
+$(document).on('click', "#clear", function(){
+$("#searchTab").removeAttr("style");
+$("#searchTab").attr("style", "display:none");
+$("#recommended-artists").removeAttr("style");
+$("#recommended-artists").attr("style", "display:none");
+$("#more").removeAttr("style");
+$("#more").attr("style", "display:none");
+$("#clear").removeAttr("style");
+$("#clear").attr("style", "display:none");
+})
+$(document).on('click', "#more", function(){
+  buildTable();
+})
